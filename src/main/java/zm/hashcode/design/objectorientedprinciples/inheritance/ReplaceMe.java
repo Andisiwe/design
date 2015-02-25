92f04506fa25a613a5ab2c8c96d3974072646bec
hashcode <boniface@hashcode.zm>
2015-02-25 09:09:02 +0200

    Exercise Structure

---
+++ src/main/java/zm/hashcode/design/objectorientedprinciples/inheritance/ReplaceMe.java
@@ -0,0 +1,7 @@
+package zm.hashcode.design.objectorientedprinciples.inheritance;
+
+/**
+ * Created by hashcode on 2015/02/25.
+ */
+public class ReplaceMe {
+}
---
+++ src/main/java/zm/hashcode/design/objectorientedprinciples/polymophism/ReplaceMe.java
@@ -0,0 +1,7 @@
+package zm.hashcode.design.objectorientedprinciples.polymophism;
+
+/**
+ * Created by hashcode on 2015/02/25.
+ */
+public class ReplaceMe {
+}
